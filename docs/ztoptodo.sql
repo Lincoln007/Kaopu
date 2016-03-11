ed8fb664721a6724abdf859293f2bae8f1235d2f
wangjianlong <810844480@qq.com>
2016-03-11 17:27:29 +0800

    数据库

--- docs/ztoptodo.sql
+++ docs/ztoptodo.sql
@@ -1,17 +1,21 @@
 -- --------------------------------------------------------
--- 主机:                           127.0.0.1
--- 服务器版本:                        5.6.26 - MySQL Community Server (GPL)
--- 服务器操作系统:                      Win64
+-- 主机:                           10.22.102.90
+-- 服务器版本:                        5.1.73-community - MySQL Community Server (GPL)
+-- 服务器操作系统:                      Win32
 -- HeidiSQL 版本:                  9.3.0.4984
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+-- 导出 ztoptodo 的数据库结构
+CREATE DATABASE IF NOT EXISTS `ztoptodo` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `ztoptodo`;
+
+
 -- 导出  表 ztoptodo.attachment 结构
-DROP TABLE IF EXISTS `attachment`;
 CREATE TABLE IF NOT EXISTS `attachment` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `FileName` varchar(128) DEFAULT NULL,
@@ -20,20 +24,12 @@ CREATE TABLE IF NOT EXISTS `attachment` (
   `SavePath` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`ID`),
   KEY `TaskID` (`TaskID`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.attachment 的数据：~4 rows (大约)
-/*!40000 ALTER TABLE `attachment` DISABLE KEYS */;
-INSERT INTO `attachment` (`ID`, `FileName`, `TaskID`, `FileSize`, `SavePath`) VALUES
-	(1, '淮北市城市建设用地节约集约利用评价项目招标文件（三次招标）.doc', 1, 119296, '635926917087643543.doc'),
-	(2, '淮北市城市建设用地节约集约利用评价项目招标文件（三次招标）.doc', 2, 119296, '635926917087643543.doc'),
-	(3, '淮北市城市建设用地节约集约利用评价项目招标文件（三次招标）.doc', 6, 119296, '635926917087643543.doc'),
-	(4, '淮北市城市建设用地节约集约利用评价项目招标文件（三次招标）.doc', 7, 119296, '635926917087643543.doc');
-/*!40000 ALTER TABLE `attachment` ENABLE KEYS */;
+-- 数据导出被取消选择。
 
 
 -- 导出  表 ztoptodo.authorizes 结构
-DROP TABLE IF EXISTS `authorizes`;
 CREATE TABLE IF NOT EXISTS `authorizes` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `GroupName` varchar(1023) DEFAULT NULL,
@@ -41,13 +37,10 @@ CREATE TABLE IF NOT EXISTS `authorizes` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.authorizes 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `authorizes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `authorizes` ENABLE KEYS */;
+-- 数据导出被取消选择。
 
 
 -- 导出  表 ztoptodo.comment 结构
-DROP TABLE IF EXISTS `comment`;
 CREATE TABLE IF NOT EXISTS `comment` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Content` varchar(50) NOT NULL,
@@ -57,19 +50,12 @@ CREATE TABLE IF NOT EXISTS `comment` (
   PRIMARY KEY (`ID`),
   KEY `TaskID` (`TaskID`),
   KEY `UserID` (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.comment 的数据：~3 rows (大约)
-/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` (`ID`, `Content`, `UserID`, `TaskID`, `CreateTime`) VALUES
-	(1, 'fsdf', 2, 4, '2016-03-04 13:39:31'),
-	(2, 'fdsfds', 2, 4, '2016-03-04 13:39:33'),
-	(3, 'fsdfds', 2, 4, '2016-03-04 13:39:34');
-/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+-- 数据导出被取消选择。
 
 
 -- 导出  表 ztoptodo.databooks 结构
-DROP TABLE IF EXISTS `databooks`;
 CREATE TABLE IF NOT EXISTS `databooks` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(255) DEFAULT NULL,
@@ -84,13 +70,10 @@ CREATE TABLE IF NOT EXISTS `databooks` (
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.databooks 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `databooks` DISABLE KEYS */;
-/*!40000 ALTER TABLE `databooks` ENABLE KEYS */;
+-- 数据导出被取消选择。
 
 
 -- 导出  表 ztoptodo.messages 结构
-DROP TABLE IF EXISTS `messages`;
 CREATE TABLE IF NOT EXISTS `messages` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Sender` varchar(1023) DEFAULT NULL,
@@ -102,13 +85,39 @@ CREATE TABLE IF NOT EXISTS `messages` (
   KEY `Sender` (`Sender`(255))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.messages 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+-- 数据导出被取消选择。
+
+
+-- 导出  表 ztoptodo.sheets 结构
+CREATE TABLE IF NOT EXISTS `sheets` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Number` varchar(255) DEFAULT NULL,
+  `NumberExt` int(11) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Time` datetime NOT NULL,
+  `Money` bit(1) NOT NULL,
+  `Status` bit(1) NOT NULL,
+  `Remarks` varchar(1023) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 数据导出被取消选择。
+
+
+-- 导出  表 ztoptodo.substancs 结构
+CREATE TABLE IF NOT EXISTS `substancs` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Category` int(2) NOT NULL DEFAULT '0',
+  `Details` varchar(1023) DEFAULT '0',
+  `Price` double DEFAULT '0',
+  `SID` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- 数据导出被取消选择。
 
 
 -- 导出  表 ztoptodo.task 结构
-DROP TABLE IF EXISTS `task`;
 CREATE TABLE IF NOT EXISTS `task` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Title` varchar(128) NOT NULL,
@@ -124,28 +133,12 @@ CREATE TABLE IF NOT EXISTS `task` (
   KEY `ScheduledTime` (`ScheduledTime`),
   KEY `Title` (`Title`),
   KEY `CreateTime` (`CreateTime`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.task 的数据：~12 rows (大约)
-/*!40000 ALTER TABLE `task` DISABLE KEYS */;
-INSERT INTO `task` (`ID`, `Title`, `Content`, `ScheduledTime`, `CreatorID`, `ParentID`, `CreateTime`, `Deleted`) VALUES
-	(1, '测试yy', '测试', '2016-03-07 00:00:00', 2, 0, '2016-03-04 12:35:08', b'1'),
-	(2, '测试yy', '测试', '2016-03-07 00:00:00', 2, 1, '2016-03-04 12:35:26', b'0'),
-	(3, '测试yy2', 'dsds', '2016-03-11 00:00:00', 2, 0, '2016-03-04 12:36:03', b'0'),
-	(4, 'cxcxc', 'xcxc', '2016-03-05 00:00:00', 2, 0, '2016-03-04 12:39:38', b'0'),
-	(5, 'xczcxzc', 'xczxcxzc', '2016-03-05 00:00:00', 2, 0, '2016-03-04 12:40:16', b'0'),
-	(6, '测试yy', '测试', '2016-03-07 00:00:00', 2, 1, '2016-03-04 12:41:56', b'0'),
-	(7, '测试yy', '测试', '2016-03-07 00:00:00', 2, 6, '2016-03-04 12:44:13', b'0'),
-	(8, 'ghhghggh', 'hgg', '2016-03-11 00:00:00', 2, 0, '2016-03-04 12:44:37', b'0'),
-	(9, 'dsdds', 'dsds', '2016-03-05 00:00:00', 7, 0, '2016-03-04 13:14:53', b'0'),
-	(10, 'dasds', 'dasdsad', '2016-03-05 00:00:00', 7, 0, '2016-03-04 13:53:40', b'0'),
-	(11, 'dasdasdsadasd', 'dasasdasd', '2016-03-11 00:00:00', 7, 0, '2016-03-04 13:55:27', b'0'),
-	(12, '测试1', '测试1', '2016-03-05 00:00:00', 8, 0, '2016-03-04 19:17:27', b'0');
-/*!40000 ALTER TABLE `task` ENABLE KEYS */;
+-- 数据导出被取消选择。
 
 
 -- 导出  表 ztoptodo.task_query 结构
-DROP TABLE IF EXISTS `task_query`;
 CREATE TABLE IF NOT EXISTS `task_query` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `UserID` int(11) NOT NULL,
@@ -155,13 +148,10 @@ CREATE TABLE IF NOT EXISTS `task_query` (
   KEY `UserID` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.task_query 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `task_query` DISABLE KEYS */;
-/*!40000 ALTER TABLE `task_query` ENABLE KEYS */;
+-- 数据导出被取消选择。
 
 
 -- 导出  表 ztoptodo.user 结构
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `GroupID` int(11) NOT NULL DEFAULT '0',
@@ -172,42 +162,27 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`ID`),
   KEY `Username` (`Username`),
   KEY `GroupID` (`GroupID`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.user 的数据：~8 rows (大约)
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`ID`, `GroupID`, `Username`, `RealName`, `LoginTimes`, `LastLoginTime`) VALUES
-	(1, 0, 'wjl', '汪建龙', 1, '2016-01-21 13:50:24'),
-	(2, 0, 'yy', '袁洋', 0, NULL),
-	(3, 0, 'ljj', '吕骏杰', 0, NULL),
-	(4, 0, 'ty', '唐尧', 0, NULL),
-	(5, 0, 'sunxy', '孙夏阳', 0, NULL),
-	(6, 0, 'zwj', '周威俊', 0, NULL),
-	(7, 0, 'slz', '测试', 0, NULL),
-	(8, 0, 'liangjun', '郑良军', 0, NULL);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+-- 数据导出被取消选择。
 
 
 -- 导出  表 ztoptodo.user_group 结构
-DROP TABLE IF EXISTS `user_group`;
 CREATE TABLE IF NOT EXISTS `user_group` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(128) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.user_group 的数据：~0 rows (大约)
-/*!40000 ALTER TABLE `user_group` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user_group` ENABLE KEYS */;
+-- 数据导出被取消选择。
 
 
 -- 导出  表 ztoptodo.user_task 结构
-DROP TABLE IF EXISTS `user_task`;
 CREATE TABLE IF NOT EXISTS `user_task` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `UserID` int(11) NOT NULL DEFAULT '0',
   `TaskID` int(11) NOT NULL DEFAULT '0',
-  `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `CreateTime` datetime NOT NULL,
   `CompletedTime` datetime DEFAULT NULL,
   `HasRead` bit(1) NOT NULL DEFAULT b'0',
   `Deleted` bit(1) NOT NULL DEFAULT b'0',
@@ -216,45 +191,12 @@ CREATE TABLE IF NOT EXISTS `user_task` (
   KEY `TaskID` (`TaskID`),
   KEY `CreateTime` (`CreateTime`),
   KEY `CompletedTime` (`CompletedTime`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 正在导出表  ztoptodo.user_task 的数据：~29 rows (大约)
-/*!40000 ALTER TABLE `user_task` DISABLE KEYS */;
-INSERT INTO `user_task` (`ID`, `UserID`, `TaskID`, `CreateTime`, `CompletedTime`, `HasRead`, `Deleted`) VALUES
-	(1, 3, 1, '2016-03-04 12:35:08', NULL, b'0', b'0'),
-	(2, 2, 1, '2016-03-04 12:35:08', NULL, b'1', b'0'),
-	(3, 5, 1, '2016-03-04 12:35:08', NULL, b'0', b'0'),
-	(4, 4, 1, '2016-03-04 12:35:08', NULL, b'0', b'0'),
-	(5, 6, 1, '2016-03-04 12:35:08', NULL, b'0', b'0'),
-	(6, 1, 1, '2016-03-04 12:35:08', NULL, b'0', b'0'),
-	(7, 5, 2, '2016-03-04 12:35:26', NULL, b'0', b'0'),
-	(8, 2, 2, '2016-03-04 12:35:26', '2016-03-04 12:38:33', b'1', b'0'),
-	(9, 5, 3, '2016-03-04 12:36:03', NULL, b'0', b'0'),
-	(10, 2, 3, '2016-03-04 12:36:03', '2016-03-04 12:38:06', b'1', b'0'),
-	(11, 2, 4, '2016-03-04 12:39:38', '2016-03-04 12:39:45', b'1', b'0'),
-	(12, 1, 5, '2016-03-04 12:40:16', NULL, b'1', b'0'),
-	(13, 2, 5, '2016-03-04 12:40:16', NULL, b'1', b'0'),
-	(14, 1, 6, '2016-03-04 12:41:56', NULL, b'0', b'0'),
-	(15, 2, 6, '2016-03-04 12:41:56', NULL, b'1', b'0'),
-	(16, 3, 7, '2016-03-04 12:44:13', NULL, b'0', b'0'),
-	(17, 2, 7, '2016-03-04 12:44:13', NULL, b'1', b'0'),
-	(18, 5, 7, '2016-03-04 12:44:13', NULL, b'0', b'0'),
-	(19, 4, 7, '2016-03-04 12:44:13', NULL, b'0', b'0'),
-	(20, 4, 8, '2016-03-04 12:44:37', NULL, b'0', b'0'),
-	(21, 2, 8, '2016-03-04 12:44:37', NULL, b'1', b'0'),
-	(22, 2, 9, '2016-03-04 13:14:53', NULL, b'0', b'0'),
-	(23, 7, 9, '2016-03-04 13:14:53', NULL, b'0', b'0'),
-	(24, 2, 10, '2016-03-04 13:53:40', NULL, b'0', b'0'),
-	(25, 7, 10, '2016-03-04 13:53:40', NULL, b'0', b'0'),
-	(26, 2, 11, '2016-03-04 13:55:27', NULL, b'0', b'0'),
-	(27, 7, 11, '2016-03-04 13:55:27', NULL, b'0', b'0'),
-	(28, 4, 12, '2016-03-04 19:17:27', NULL, b'0', b'0'),
-	(29, 8, 12, '2016-03-04 19:17:27', NULL, b'1', b'0');
-/*!40000 ALTER TABLE `user_task` ENABLE KEYS */;
+-- 数据导出被取消选择。
 
 
 -- 导出  视图 ztoptodo.user_task_view 结构
-DROP VIEW IF EXISTS `user_task_view`;
 -- 创建临时表以解决视图依赖性错误
 CREATE TABLE `user_task_view` (
 	`ID` INT(11) NOT NULL,
@@ -270,7 +212,6 @@ CREATE TABLE `user_task_view` (
 
 
 -- 导出  视图 ztoptodo.user_task_view 结构
-DROP VIEW IF EXISTS `user_task_view`;
 -- 移除临时表并创建最终视图结构
 DROP TABLE IF EXISTS `user_task_view`;
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`::1` VIEW `user_task_view` AS SELECT
