175f8541b536b357a178c4e399d27a6059ec5675
wangjianlong <810844480@qq.com>
2016-09-20 17:57:54 +0800

    开始平板管理系统制作

---
+++ Ztop.Todo.Model/iPad.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ztop.Todo.Model
+{
+    public class iPad
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public string SerialNumber { get; set; }
+        public iPadType Type { get; set; }
+        public Space Space { get; set; }
+
+    }
+
+
+    public enum iPadType
+    {
+        [Description("iPad Mini2")]
+        Mini2,
+        [Description("iPad Mini4")]
+        Mini4,
+        [Description("iPad Air")]
+        Air,
+        [Description("iPad Air2")]
+        Air2,
+        [Description("iPad Pro")]
+        Pro
+    }
+
+    public enum Space
+    {
+        [Description("16G")]
+        Sixteen,
+        [Description("32G")]
+        ThirtyTwo,
+        [Description("64G")]
+        SixtyFour,
+        [Description("128G")]
+        OneHundredAndTwentyEight,
+        [Description("256G")]
+        TwoHundredAndFivetySix
+    }
+}
---
+++ Ztop.Todo.Web/Controllers/iPadController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ztop.Todo.Web.Controllers
+{
+    public class iPadController : ControllerBase
+    {
+        // GET: iPad
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string title)
+        {
+            return SuccessJsonResult();
+        }
+    }
+}
